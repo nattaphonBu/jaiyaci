f73dbd186a0b77f4090f4635d3034cccfd6c87fd
Yodev <natlove.natlove202@gmail.com>
2018-09-10 23:07:33 +0700

    update

---
+++ application/views/register/register.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>
+    <form id="insert">
+        <center>
+            <div class="col-md-4 mb-3">
+                <div class="form-group">
+                    <p align="LEFT">
+                        <label>ID</label>
+                        <input type="text" class="form-control" id="id"  placeholder="ID">
+                    
+                        <label>email</label>
+                        <input type="email" class="form-control" id="email" placeholder="email">
+                    
+                        <label>Password</label>
+                        <input type="password" class="form-control" id="password" placeholder="Password">
+                    
+                        <label>Confirm Password</label>
+                        <input type="password" class="form-control" id="confirmpassword" placeholder="confirm password">
+                    
+                        <label>Telephone</label>
+                        <input type="text" class="form-control" id="tel" placeholder="telephone">
+                    
+                    
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </p>
+                </div>
+             </div>
+        </center>
+    </form>
+
+    <!-- <form id="insert">
+        Groupname: <input type="text" name="groupname" id="groupname">
+        Service: <input type="text" name="service" id="service1">
+                 <input type="text" name="service" id="service2">
+        <input type="submit" value="send">
+
+    </form> -->
+
+    <script src="./js/jquery-3.3.1.min.js"></script>
+    <script src="./js/setup.js"></script>
+    <script>
+        $(function(){
+            $("#insert").submit(function (e) { 
+                e.preventDefault();
+                var formdata = {
+                    "id": $("#id").val(),
+                "email": $("#email").val(),
+                "password": $("#password").val(),
+                "confirmpassword": $("#confirmpassword").val(),
+                "tel": $("#tel").val()
+                };
+                // console.log(formdata);
+                $.post("http://localhost:8080/Project/api/jaiya/insert", JSON.stringify(formdata),
+                    function (data, textStatus, jqXHR) {
+                        alert(data.message);
+                    }
+                );
+                    
+            });
+        });
+    
+    </script>
+</body>
+</html>
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,89 +1,79 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
-
-	<style type="text/css">
-
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
-
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
-
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
-
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
-
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-
-	#body {
-		margin: 0 15px 0 15px;
-	}
-
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <title>Document</title>
 </head>
 <body>
+	
+    <form id="insert">
+        <center>
+			<h1>Register</h1>
+            <div class="col-md-4 mb-3">
+                <div class="form-group">
+                    <p align="LEFT">
+                        <label>ID</label>
+                        <input type="text" class="form-control" id="id"  placeholder="ID">
+                    
+                        <label>email</label>
+                        <input type="email" class="form-control" id="email" placeholder="email">
+                    
+                        <label>Password</label>
+                        <input type="password" class="form-control" id="password" placeholder="Password">
+                    
+                        <label>Confirm Password</label>
+                        <input type="password" class="form-control" id="confirmpassword" placeholder="confirm password">
+                    
+                        <label>Telephone</label>
+                        <input type="text" class="form-control" id="tel" placeholder="telephone">
+                    
+						<div class="col-lg-4">
+						<button type="submit" class="btn btn-primary">Submit</button>
+						</div>
+                    </p>
+                </div>
+             </div>
+        </center>
+    </form>
 
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+    <!-- <form id="insert">
+        Groupname: <input type="text" name="groupname" id="groupname">
+        Service: <input type="text" name="service" id="service1">
+                 <input type="text" name="service" id="service2">
+        <input type="submit" value="send">
 
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+    </form> -->
 
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-	</div>
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
-</div>
-
+    <script src="./js/jquery-3.3.1.min.js"></script>
+    <script src="./js/setup.js"></script>
+    <script>
+        $(function(){
+            $("#insert").submit(function (e) { 
+                e.preventDefault();
+                var formdata = {
+                    "id": $("#id").val(),
+                "email": $("#email").val(),
+                "password": $("#password").val(),
+                "confirmpassword": $("#confirmpassword").val(),
+                "tel": $("#tel").val()
+                };
+                // console.log(formdata);
+                $.post("http://localhost:8080/Project/api/jaiya/insert", JSON.stringify(formdata),
+                    function (data, textStatus, jqXHR) {
+                        alert(data.message);
+                    }
+                );
+                    
+            });
+        });
+    
+    </script>
 </body>
 </html>
