f25cadef1a6c16b51209491cf5c89f06d2bc79f9
Yodev <natlove.natlove202@gmail.com>
2018-09-26 17:13:24 +0700

    jaiya

--- application/views/register/register.php
+++ application/views/register/register.php
@@ -47,31 +47,7 @@
         <input type="submit" value="send">
 
     </form> -->
-
+<!-- 
     <script src="./publish/js/jquery-3.3.1.min.js"></script>
-    <script src="./publish/js/setup.js"></script>
-    <script>
-        $(function(){
-            $("#insert").submit(function (e) { 
-                e.preventDefault();
-                var formdata = {
-                    "username": $("#username").val(),
-                "email": $("#email").val(),
-                "password": $("#password").val(),
-                // "confirmpassword": $("#confirmpassword").val(),
-                "tel": $("#tel").val()
-                };
-                // console.log(formdata);
-                $.post("http://localhost:8080/Project/api/jaiya/insert", JSON.stringify(formdata),
-                    function (data, textStatus, jqXHR) {
-                        alert(data.message);
-                    }
-                );
-                    
-            });
-        });
+    <script src="./publish/js/setup.js"></script> -->
     
-    </script>
-   
-</body>
-</html>
