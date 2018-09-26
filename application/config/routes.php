d5629e016611e17cdac67d7cd98e06dcee0ce6df
Yodev <natlove.natlove202@gmail.com>
2018-09-26 20:56:01 +0700

    add menu

--- application/config/routes.php
+++ application/config/routes.php
@@ -53,4 +53,5 @@ $route['default_controller'] = 'login';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 $route['register'] = 'Register/register';
+$route['menu'] ='Menu/menu';
 
