4ea44e78a104156e32517ae1232f55b5dd23e010
Yodev <natlove.natlove202@gmail.com>
2018-09-28 16:56:16 +0700

    add porgram import/export dbmongo

---
+++ db/import mongodb.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main () {
+
+		system("mongoimport -d jaiya -c province --drop --file province.json");
+		system("mongoimport -d jaiya -c district --drop --file district.json");
+		system("mongoimport -d jaiya -c hospital --drop --file hospital.json");
+		system("mongoimport -d jaiya -c machine --drop --file machine.json");
+		system("mongoimport -d jaiya -c subdistrict --drop --file subdistrict.json");
+		system("mongoimport -d jaiya -c user --drop --file user.json");
+		system("mongoimport -d jaiya -c userData --drop --file userData.json");
+		system("mongoimport -d jaiya -c timetogetpillow --drop --file timetogetpillow.json");
+		
+	
+
+   return(0);
+} 
