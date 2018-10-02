ca4a213d54de17c3f10bb266c14ed9f056c21ad3
Yodev <natlove.natlove202@gmail.com>
2018-10-02 21:37:58 +0700

    commit validate

--- application/views/layout/foot.php
+++ application/views/layout/foot.php
@@ -1,4 +1,4 @@
 </div>
     <script src="<?=base_url("/publish/js/jquery-3.3.1.min.js") ?>"></script> 
     <script src="<?=base_url("/publish/js/setup.js") ?>"></script>
-</script>
+    <script src="<?php echo base_url() ?>public/js/jquery.validate.min.js"></script>
--- application/views/user/register/create/script.php
+++ application/views/user/register/create/script.php
@@ -1,23 +1,55 @@
 <script>
-    $(function(){
-        $("#insert").submit(function (e) { 
-            e.preventDefault();
-            var formdata = {
-                "username": $("#username").val(),
-            "email": $("#email").val(),
-            "password": $("#password").val(),
-            // "confirmpassword": $("#confirmpassword").val(),
-            "tel": $("#tel").val()
-            };
-            // console.log(formdata);
-            $.post("http://localhost:8080/JaiyaSrc/api/register/insert", JSON.stringify(formdata),
-                function (data, textStatus, jqXHR) {
-                    alert(data.message);
-                }
-            );
+    // $(function(){
+    //     $("#insert").submit(function (e) { 
+    //         e.preventDefault();
+    //         var formdata = {
+    //             "username": $("#username").val(),
+    //         "email": $("#email").val(),
+    //         "password": $("#password").val(),
+    //         // "confirmpassword": $("#confirmpassword").val(),
+    //         "tel": $("#tel").val()
+    //         };
+    //         // console.log(formdata);
+    //         $.post("http://localhost:8080/JaiyaSrc/api/register/insert", JSON.stringify(formdata),
+    //             function (data, textStatus, jqXHR) {
+    //                 alert(data.message);
+    //             }
+    //         );
                 
-        });
+    //     });
+    // });
+    $("#submit").validate({
+        rules: {
+            username: {
+                required: true
+            },
+        },
+        messages: {
+            username: {
+                required: "กรุณากรอกชื่อ"
+            }
+        },
     });
+    
+    $("#submit").submit(function(){
+        createRim();
+    })
+
+
+    function createRim(){
+        event.preventDefault();
+        var isValid = $("#submit").valid();
+        
+        if(isValid){
+            var data = $("#submit").serialize();
+            $.post("http://localhost:8080/JaiyaSrc/api/register/insert", JSON.stringify(formdata),
+            function(data){
+                alert(data.message);
+            });
+            
+        }
+    }
+    
 
 </script>
    
