f08292b5f5a2ff62e7b5eebe243dc83eed4071be
arifeen008 <feenkundee@gmail.com>
2018-10-02 22:03:47 +0700

    Configuration login but unsuccess

--- application/views/login/content.php
+++ application/views/login/content.php
@@ -1,7 +1,44 @@
-<<<<<<< HEAD
+<!-- <<<<<<< HEAD -->
 <!DOCTYPE html>
 <html lang="en">
+<head>
+<style>
+.button {
+    background-color: #4CAF50;
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+}
+
+#example1 {
+    border: 10px dotted black;
+    padding:35px;
+    background: yellow;
+}
+
+#example2 {
+    border: 10px dotted black;
+    padding:35px;
+    background: yellow;
+    background-clip: padding-box;
+}
+
+#example3 {
+    border: 10px dotted black;
+    padding:35px;
+    background: yellow;
+    background-clip: content-box;
+}
+</style>
+</head>
 <body>
+<div id="example3">
     <form id="insert">
     <div class="card-body black bg-light">
         <center>
@@ -15,7 +52,7 @@
                         <label>Password</label><span class="error"></span>
                         <input type="password" class="form-control" id="password" placeholder="Password">                
                         <br>                     
-                            <button>ลืมรหัสผ่าน</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button>สมัครสมาชิก</button>
+                            <p align="CENTER"><button class="button">ลืมรหัสผ่าน</button>&nbsp;<button class="button">สมัครสมาชิก</button></p>
 						<div class="col-lg-4">
 						<a class="item" href="<?=base_url("/Register/register") ?>">ลงชื่อเข้าใช้</a>
 						</div>
@@ -27,9 +64,9 @@
 		</center>
     </div>	
 	</form>
-    
+</div>  
 
-
-=======
-<a class="item" href="<?=base_url("register/register") ?>">ลงชื่อเข้าใช้</a>
->>>>>>> 38df11ba8523e311bb36212a863b25eb98a652d2
+</html>
+<!-- =======
+<a class="item" href="<?=base_url("register/register") ?>">ลงชื่อเข้าใช้</a> -->
+<!-- >>>>>>> 38df11ba8523e311bb36212a863b25eb98a652d2 -->
