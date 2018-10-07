7d994e616274d788a84e9dcfc0550c77ffd110d9
Yodev <natlove.natlove202@gmail.com>
2018-10-07 21:03:35 +0700

    edit pge menu

--- application/views/user/menu/content.php
+++ application/views/user/menu/content.php
@@ -1,18 +1,95 @@
-
+<section class="welcome_area clearfix" id="" style="background-image: url(../publish/img/welcome-bg.png)" width="80%">
 <div class="container">
+<form id="insert">
+    <div class="row justify-content-md-center">
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="card text-white bg-warning">
+                <div class="card-header">
+                  <h3 class="card-title"></i> แจ้งเตือนกินยา</h3>
+                </div>
+                
+                
+                  <div class="card-body black bg-light">
+                    <div class="row">
+                    <div class="col-md-4">
+                        <div class="form-group">
+                          <label class="text-dark">วันที่</label> 
+                          <input type="text" class="form-control" placeholder="10/7/2561" name="firstName" id="firstName">
+                        </div>
+                      </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                          <label class="text-dark">เวลา</label> 
+                          <input type="text" class="form-control" placeholder="8.00" name="firstName" id="firstName">
+                        </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="form-group">
+                          <label class="text-dark">ช่วงที่กิน</label> 
+                          <input type="text" class="form-control" placeholder="หลังอาหาร" name="firstName" id="firstName">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                      <button type="submit" class="btn btn-success">กินแล้ว</button>
+                    </div>
+                      </div>
+                    </div> 
+                   
+                  
+               
+                 
+               
+            </div>
+            
+            <div class="card-body black bg-light">
+              <div class="row">
+              <div class="col-md-4">
+                  <div class="form-group">
+                    <label class="text-dark">วันที่</label> 
+                    <input type="text" class="form-control" placeholder="10/7/2561" name="firstName" id="firstName">
+                  </div>
+                </div>
+              <div class="col-md-4">
+                  <div class="form-group">
+                    <label class="text-dark">เวลา</label> 
+                    <input type="text" class="form-control" placeholder="8.00" name="firstName" id="firstName">
+                  </div>
+                </div>
+                <div class="col-md-4">
+                  <div class="form-group">
+                    <label class="text-dark">ช่วงที่กิน</label> 
+                    <input type="text" class="form-control" placeholder="หลังอาหาร" name="firstName" id="firstName">
+                  </div>
+                </div>
+                <div class="form-group">
+                      <button type="submit" class="btn btn-success">กินแล้ว</button>
+                </div>
+                </div>
+              </div> 
+        </div>
+        </div>
+      </div>
+    </section>
+    </div>
+    </form>
+</div>
+</section>
    <br><center><h1>Menu</h1><br>
     <div class="row">
         <a class="col-md-3" href="<?=base_url("") ?>">
-            <img src="./publish/img/alarm.png" class="img-responsive" width="200px" height="200px"><p>แจ้งเตือน</p>
+            <img src="../publish/img/alarm.png" class="img-responsive" width="200px" height="200px"><p class="h3">แจ้งเตือน</p>
+        </a>
+        <a class="col-md-3" href="<?=base_url("user/userdata/insert") ?>">
+            <img src="../publish/img/edit profile.png" class="img-responsive" width="200px" height="200px"><p class="h3">บัญชีผู้ใช้</p>
         </a>
         <a class="col-md-3" href="<?=base_url("") ?>">
-            <img src="./publish/img/edit profile.png" class="img-responsive" width="200px" height="200px"><p>บัญชีผู้ใช้</p>
+            <img src="../publish/img/history.png" class="img-responsive" width="200px" height="200px"><p class="h3">ประวัติยา</p>
         </a>
         <a class="col-md-3" href="<?=base_url("") ?>">
-            <img src="./publish/img/history.png" class="img-responsive" width="200px" height="200px"><p>ประวัติยา</p>
-        </a>
-        <a class="col-md-3" href="<?=base_url("") ?>">
-            <img src="./publish/img/marker.png" class="img-responsive" width="200px" height="200px"><p>ค้นหาโรงพยาบาล</p>
-        </a>
-    </div>  
+            <img src="../publish/img/marker.png" class="img-responsive" width="200px" height="200px" ><p class="h3">ค้นหาโรงพยาบาล</p>
+        </a> 
+     </div>  
 </div>
