1bb2f18bcba0348997d0028e2b122b0409090a99
Yodev <natlove.natlove202@gmail.com>
2018-10-07 20:09:06 +0700

    page data

--- application/views/user/layout/foot.php
+++ application/views/user/layout/foot.php
@@ -1,5 +1,6 @@
 </div>
-<script src="<?=base_url("/publish/js/jquery-3.3.1.min.js") ?>"></script> 
+<script src="<?php echo base_url() ?>publish/js/jquery-3.3.1.min.js"></script> 
+
 
 
 <script src="<?=base_url("/publish/js/setup.js") ?>"></script>
