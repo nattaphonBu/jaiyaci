8e5cfe3527dde7ae35b6d0fce9237451a5e6a200
Yodev <natlove.natlove202@gmail.com>
2018-10-10 17:47:55 +0700

    login

--- application/views/login/content.php
+++ application/views/login/content.php
@@ -1,41 +1,45 @@
 
 <body>
     <form id ="login">
-</div>
-    <div class="row justify-content-md-center">
-        <div class="col-md-4 mb-4">
-            <div class="form-group">
-                
-                    <label class="">ชื่อผู้ใช้</label><span class="error">*</span>
-                    <input type="text" class="form-control" id="username" name="username" placeholder="username">
+    <div class="container-fluid">
+        <div class="row">
+        <div class="col-md-5 col-md-offset-5">
+            <div class="intro-text">
+                <h1>Login</h1>
             </div>  
+        </div> 
+            <div class="col-md-4 col-md-offset-4">
+             
+           
+            <!-- <div class="col-md-4 mb-4"> -->
+                <div class="form-group"> 
+                    <div class="intro-text">
+                    <label class="h5">ชื่อผู้ใช้</label><span class="error">*</span>
+                    <input type="text" class="form-control" id="username" name="username" placeholder="username">
+                </div>
+                </div>
+            </div>  
+            <div class="col-md-4 col-md-offset-4">
+            <div class="form-group"> 
+                <div class="form-group">
+                    <label class="h5">รหัสผ่าน</label><span class="error">*</span>
+                    <input type="password" class="form-control" id="password" name="password" placeholder="Password">           
+                </div>  
+            </div>
+            </div>
+            <div class="col-md-5 col-md-offset-5">
+                <div class="form-group">
+                    <button type="insert" class="btn-lg btn-success">เข้าสู่ระบบ</button>
+                </div>
+            </div>
+            <div class="col-md-5 col-md-offset-5">
+                <div class="form-group">
+                    <a class=" btn btn-lg btn-info" href="<?=base_url("Register/insert") ?>"><i>สมัครสมาชิก</i></a>
+                </div>
+            </div>
+            </div>
         </div>
     </div>
-        <div class="row justify-content-md-center">
-        <div class="col-md-4 mb-4">
-            <div class="form-group">
-                    <label class="">รหัสผ่าน</label><span class="error">*</span>
-                    <input type="password" class="form-control" id="password" name="password" placeholder="Password">           
-            </div>  
-        </div>    
-    </div>
-    <div class="row justify-content-md-center">
-    <button type="insert" class="btn-lg btn-success">เข้าสู่ระบบ</button>
-</div>
-<br>
-<div class="row justify-content-md-center">
-    <div>
-        <a class=" btn btn-lg btn-info" href="<?=base_url("Register/insert") ?>">
-            <i>สมัครสมาชิก</i>
-        </a>
-    </div>
-    <!-- <div>
-    <button type="button" class="btn-lg btn-danger">forgot password</button>
-    </div> -->
-</div>
-    </section> 
-</form>
-</div>
+    </form>
 </body>
 
-</html>
