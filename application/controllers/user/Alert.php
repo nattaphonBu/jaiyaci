5698fa035fcdf04db4147260ccb475b5ae85b42a
DESKTOP-U4NNI57\Geforce <sidaratsamee@gmail.com>
2018-10-15 14:49:45 +0700

    alert

--- application/controllers/user/Alert.php
+++ application/controllers/user/Alert.php
@@ -12,9 +12,9 @@ class Alert extends CI_Controller {
 		$this->load->view("template/head");
 		$this->load->view("template/header");
 		$this->load->view("template/banner");
-		$this->load->view("template/headUsermenu");
+		// $this->load->view("template/headUsermenu");
 		$this->load->view("user/alert/content");
-		$this->load->view("template/footUsermenu");
+		// $this->load->view("template/footUsermenu");
 		$this->load->view("template/footer");
 		$this->load->view("template/foot");	
 		$this->load->view("user/alert/script");
