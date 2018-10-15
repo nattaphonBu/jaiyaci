28100ee2bba59283ccd1606fe49e5d233b789e0e
DESKTOP-U4NNI57\Geforce <sidaratsamee@gmail.com>
2018-10-15 09:20:18 +0700

    add link

--- application/controllers/user/Alert.php
+++ application/controllers/user/Alert.php
@@ -9,13 +9,15 @@ class Alert extends CI_Controller {
     }
 	public function alert()
 	{
-		$this->load->view("layout/head");
-		$this->load->view("layout/header");
+		$this->load->view("template/head");
+		$this->load->view("template/header");
+		$this->load->view("template/banner");
+		$this->load->view("template/headUsermenu");
 		$this->load->view("user/alert/content");
-		$this->load->view("layout/footer");
-		$this->load->view("layout/foot");	
+		$this->load->view("template/footUsermenu");
+		$this->load->view("template/footer");
+		$this->load->view("template/foot");	
 		$this->load->view("user/alert/script");
-		
 	}
 	
 }
--- application/views/user/menu/content.php
+++ application/views/user/menu/content.php
@@ -6,14 +6,13 @@
 				<button class="cn-button" id="cn-button">Menu</button>
 				<div class="cn-wrapper" id="cn-wrapper">
 					<ul>
-						<!-- <li><a href="#"><span>Home</span></a></li> -->
+						<li><a href="<?=base_url("user/Menu/menu") ?>"><span>Home</span></a></li>
 						<li><a href="<?=base_url("user/Alert/Alert") ?>"><span>แจ้งเตือน</span></a></li>
 						<li><a href="<?=base_url("user/Userdata/insert") ?>"><span>บัญชีผู้ใช้</span></a></li>
 						<li><a href="<?=base_url("user/History") ?>"><span>ประวัติการกินยา</span></a></li>
 						<li><a href="<?=base_url("user/Hospital") ?>"><span>ค้นหาโรงพยาบาล</span></a></li>
 						<li><a href="<?=base_url("user/Timetogetpillow/insert") ?>"><span>ตั้งเวลากินยา</span></a></li>
-						<!-- <li><a href="#"><span>ตั้งค่า</span></a></li> -->
-					 </ul>
+						<li><a href="<?=base_url("login/logout") ?>"><span>ลงชื่อออก</span></a></li>
 				</div>
 				<!-- End of Nav Structure -->
 			</div>
