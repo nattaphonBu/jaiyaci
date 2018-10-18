f2d8941dc93afff4a72149f60d5ebb03e1c6ec08
sidaratkhaobang <slotsnow0301@gmail.com>
2018-10-18 21:22:32 +0700

    add insert

--- application/views/admin/machine/create/content.php
+++ application/views/admin/machine/create/content.php
@@ -8,76 +8,55 @@
                 <h1>Create Machine</h1>
             </div>  
         </div> 
-            <!-- <div class="col-md-4 col-md-offset-4"> -->
-             
-            <!-- <div class="col-md-4 mb-4"> -->
-                <!-- <div class="form-group"> 
-                    <div class="intro-text">
-                    <label class="h5">userid</label><span class="error">*</span>
-                    <input type="text" class="form-control" id="username" name="username" placeholder="username">
-                </div>
-                </div>
-            </div>   -->
             <div class="col-md-4 col-md-offset-4">
             <div class="form-group"> 
                 <div class="form-group">
-                    <label class="h5">NameofMachine</label><span class="error">*</span>
-                    <input type="text" class="form-control" id="" name="" placeholder="NameofMachine">           
+                    <label class="h5">หมายเลขเครื่อง</label><span class="error">*</span>
+                    <input type="text" class="form-control" id="nameofmachine" name="nameofmachine" placeholder="หมายเลขเครื่อง">           
                 </div>  
             </div>
             </div>
             <div class="col-md-4 col-md-offset-4">
             <div class="form-group"> 
                 <div class="form-group">
-                    <label class="h5">latitude</label><span class="error">*</span>
-                    <input type="text" class="form-control" id="" name="" placeholder="latitude">           
+                    <label class="h5">ละติจูด</label><span class="error">*</span>
+                    <input type="text" class="form-control" id="latitude" name="latitude" placeholder="ละติจูด">           
                 </div>  
             </div>
             </div>
             <div class="col-md-4 col-md-offset-4">
             <div class="form-group"> 
                 <div class="form-group">
-                    <label class="h5">longitude</label><span class="error">*</span>
-                    <input type="text" class="form-control" id="password" name="password" placeholder="longitude">           
+                    <label class="h5">ลองติจูด</label><span class="error">*</span>
+                    <input type="text" class="form-control" id="longjijude" name="longjijude" placeholder="ลองติจูด">           
+                </div>  
+            </div>
+            </div>
+            
+            <div class="col-md-4 col-md-offset-4">
+            <div class="form-group"> 
+                <div class="form-group">
+                    <label class="h5">อำเภอ</label><span class="error">*</span>
+                    <input type="text" class="form-control" id="districtId" name="districtId" placeholder="อำเภอ">           
                 </div>  
             </div>
             </div>
             <div class="col-md-4 col-md-offset-4">
             <div class="form-group"> 
-                <div class="text-dark">
-                    <label class="h5">province</label><span class="error">*</span>
-                    <select class="form-control" name="" id="">
-                    <option value="">province</option>
-                    <option value="province">province</option>
-                    <option value="province">province</option>
-                    </select>
+                <div class="form-group">
+                    <label class="h5">จังหวัด</label><span class="error">*</span>
+                    <input type="text" class="form-control" id="provinceId" name="provinceId" placeholder="จังหวัด">           
                 </div>  
             </div>
             </div>
-            <div class="col-md-4 col-md-offset-4">
+            <!-- <div class="col-md-4 col-md-offset-4">
             <div class="form-group"> 
-                <div class="text-dark">
-                    <label class="h5">district</label><span class="error">*</span>
-                    <select class="form-control" name="" id="">
-                    <option value="">district</option>
-                    <option value="district">district</option>
-                    <option value="district">district</option>
-                    </select>
+                <div class="form-group">
+                    <label class="h5">subdistrictId</label><span class="error">*</span>
+                    <input type="text" class="form-control" id="subdistrictId" name="subdistrictId" placeholder="subdistrictId">           
                 </div>  
             </div>
-            </div>
-            <div class="col-md-4 col-md-offset-4">
-            <div class="form-group"> 
-                <div class="text-dark">
-                    <label class="h5">subdistricts</label><span class="error">*</span>
-                    <select class="form-control" name="" id="">
-                    <option value="">subdistricts</option>
-                    <option value="subdistricts">subdistricts</option>
-                    <option value="subdistricts">subdistricts</option>
-                    </select>
-                </div>  
-            </div>
-            </div>
+            </div> -->
 
             <div class="col-md-5 col-md-offset-5">
                 <div class="form-group">
